48933133dda5b4fe3a0d5b377ef9284fd01d8af3
raulestevam <estevamraul07@gmail.com>
2026-08-05 20:40:49 -0300

    criado arquivo main e criado objeto petal referenciando a class petal

--- 08-05 introducao_poob/src/meuJardim/Main.java
+++ 08-05 introducao_poob/src/meuJardim/Main.java
@@ -1,1 +1,26 @@
+package meuJardim;
 
+public class Main{
+    public static void main(String[] args){
+        petal petal01=null;
+
+        System.out.println(petal01);
+
+        petal01 = new petal();//instanciação
+
+        System.out.println(petal01);
+
+        petal01.setColor("rosa");
+
+        petal01.printColor();
+
+        petal petal02=new petal();//instancição
+        System.out.println(petal02);
+        petal02.setColor("white");
+        petal02.printColor();
+
+        petal02 = petal01; //petal02 pegou o endereço de memoria de petal01, apontam para o msm lugar
+        petal02.printColor();
+        petal01.printColor();
+    }
+}
