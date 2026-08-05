d1051ebb51d4522c1583400ca70b2831cfdffc55
raulestevam <estevamraul07@gmail.com>
2026-08-05 20:40:28 -0300

    criado classe petal

---
+++ 08-05 introducao_poob/src/meuJardim/petal.java
@@ -0,0 +1,12 @@
+package meuJardim;
+
+public class petal {
+    private String color="";
+
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void printColor(){
+        System.out.println(color);
+    }
+}
