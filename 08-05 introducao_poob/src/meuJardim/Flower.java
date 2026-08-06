5aec06fbbfd74a6a58f9cf26e4e52edae936b887
raulestevam <estevamraul07@gmail.com>
2026-08-05 22:25:40 -0300

    adicionado novas features em flower

--- 08-05 introducao_poob/src/meuJardim/Flower.java
+++ 08-05 introducao_poob/src/meuJardim/Flower.java
@@ -4,6 +4,14 @@ public class Flower {
     private String name;
     private Petal petal1;
     private Petal petal2;
+    Petal[] petals = new Petal[3];
+    int index=0;
+
+    public void addPetal(Petal petal){
+        petals[index]=petal;
+        index++;
+        if(index<2) index=0;
+    }
 
     public Flower(Petal petal1, Petal petal2){
         this.petal1=petal1;
@@ -19,5 +27,14 @@ public class Flower {
         System.out.println("Petal color: ");
         petal1.printColor();
         petal2.printColor();
+    }
+
+    public void printFlowerVector(){
+        System.out.print("Flower: ");
+        System.out.println(name);
+        System.out.println("Petal color: ");
+        for (int i=0; i<index; i++){
+            petals[i].printColor();
+        }
     }
 }
