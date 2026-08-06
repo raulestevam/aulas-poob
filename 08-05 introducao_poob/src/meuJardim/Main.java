46ddf95baca913e6c5c91daeae3c0718e684ea43
raulestevam <estevamraul07@gmail.com>
2026-08-05 22:26:33 -0300

    organizado objetos em main, criado jardim que adiciona os objetos em uma única chamada

--- 08-05 introducao_poob/src/meuJardim/Main.java
+++ 08-05 introducao_poob/src/meuJardim/Main.java
@@ -2,25 +2,35 @@ package meuJardim;
 
 public class Main{
     public static void main(String[] args){
-        petal petal01=null;
+        Petal petal01= new Petal();//instanciação
+        petal01.setColor("pink");
 
-        System.out.println(petal01);
-
-        petal01 = new petal();//instanciação
-
-        System.out.println(petal01);
-
-        petal01.setColor("rosa");
-
-        petal01.printColor();
-
-        petal petal02=new petal();//instancição
-        System.out.println(petal02);
+        Petal petal02=new Petal();//instancição
         petal02.setColor("white");
-        petal02.printColor();
 
-        petal02 = petal01; //petal02 pegou o endereço de memoria de petal01, apontam para o msm lugar
-        petal02.printColor();
-        petal01.printColor();
+        Petal petal03=new Petal();//instancição
+        petal03.setColor("blue");
+
+        Petal petal04=new Petal();//instancição
+        petal04.setColor("purple");
+
+        //petal02 = petal01; //petal02 pegou o endereço de memoria de petal01, apontam para o msm lugar
+
+//        Flower pink = new Flower(petal01, petal02);
+        Flower pink = new Flower();
+        pink.setName("pink");
+        pink.addPetal(petal01);
+        pink.addPetal(petal02);
+
+//      Flower blossom = new Flower(petal03, petal04);
+        Flower blossom = new Flower();
+        blossom.setName("Blossom");
+        blossom.addPetal(petal03);
+        blossom.addPetal(petal04);
+
+        Garden garden = new Garden();
+        garden.setName("Hanging Gardens");
+        garden.setFlower(pink, blossom);
+        garden.printGarden();
     }
 }
