a2bba709ce31a537fdf883d6f786c3b9b0cee71a
raulestevam <estevamraul07@gmail.com>
2026-08-12 19:45:41 -0300

    criado exemplo de encapsulamento

---
+++ 08-12 encapsulamento/src/Encapsulamento/Encapsulado.java
@@ -0,0 +1,13 @@
+package Encapsulamento;
+
+public class Encapsulado {
+    private int value;
+
+    public void setValue(int value){
+        if (value>10000) return;
+        this.value=value;
+    }
+    public int getValue(){
+        return value;
+    }
+}
