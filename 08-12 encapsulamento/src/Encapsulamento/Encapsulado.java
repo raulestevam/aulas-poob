65202936ade472925abe79b380518ab85b8bee49
raulestevam <estevamraul07@gmail.com>
2026-08-12 19:46:04 -0300

    a

--- 08-12 encapsulamento/src/Encapsulamento/Encapsulado.java
+++ 08-12 encapsulamento/src/Encapsulamento/Encapsulado.java
@@ -4,7 +4,8 @@ public class Encapsulado {
     private int value;
 
     public void setValue(int value){
-        if (value>10000) return;
+        if (value>10000)
+            return;
         this.value=value;
     }
     public int getValue(){
