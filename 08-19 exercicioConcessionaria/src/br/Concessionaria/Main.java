e31eda9ea078a1ead3ee26a59265c8849a3ae9a3
raulestevam <estevamraul07@gmail.com>
2026-08-19 22:07:46 -0300

    atualizado arquivos main

--- 08-19 exercicioConcessionaria/src/br/Concessionaria/Main.java
+++ 08-19 exercicioConcessionaria/src/br/Concessionaria/Main.java
@@ -5,22 +5,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Garagem garagem = new Garagem();
-
         Menu menu = new Menu();
+        Scanner leitor = new Scanner(System.in);
 
-        int opcao = 0;
+        int opcao = -1;
 
-        while(opcao!=5){
-            opcao=menu.mostrarMenu();
+        while(opcao!=0){
+            opcao = menu.mostrarMenu();
+
             if(opcao==1){
                 garagem.createCarro();
             } else if (opcao==2) {
-                nav.navegarSul();
+                garagem.listarCarros();
             } else if (opcao==3) {
-                nav.navegarSul();
+                System.out.println("Digite o id do carro que deseja buscar:");
+                int id = leitor.nextInt();
+                Carro carro = garagem.buscaCarro(id);
+                garagem.readCarro(carro);
             } else if (opcao==4) {
-                nav.navegarNorte();
+                System.out.println("Digite o id do carro que deseja atualizar:");
+                int id = leitor.nextInt();
+                garagem.updateCarro(id);
+            } else if (opcao==5) {
+                System.out.println("Digite o id do carro que deseja excluir:");
+                int id = leitor.nextInt();
+                garagem.deleteCarro(id);
+            } else if (opcao!=0) {
+                System.out.println("Opcao invalida!");
             }
         }
+
+        System.out.println("Saindo...");
     }
 }
