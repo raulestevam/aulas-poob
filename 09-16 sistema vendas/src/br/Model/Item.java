b43b428b6669b65bb5639765ff8b7d28efb6c497
Raul Estevam <166710192+raulestevam@users.noreply.github.com>
2026-09-20 19:10:31 -0300

    Corrige modelos e DAOs do sistema de vendas

--- 09-16 sistema vendas/src/br/Model/Item.java
+++ 09-16 sistema vendas/src/br/Model/Item.java
@@ -1,12 +1,25 @@
 package br.Model;
 
 public class Item {
+    private int id;
     private Produto produto;
-    private int qtd;
+    private int quantidade;
 
-    public Item(Produto produto, int qtd) {
+    public Item() {
+    }
+
+    public Item(int id, Produto produto, int quantidade) {
+        this.id = id;
         this.produto = produto;
-        this.qtd = qtd;
+        setQuantidade(quantidade);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Produto getProduto() {
@@ -17,11 +30,29 @@ public class Item {
         this.produto = produto;
     }
 
-    public int getQtd() {
-        return qtd;
+    public int getQuantidade() {
+        return quantidade;
     }
 
-    public void setQtd(int qtd) {
-        this.qtd = qtd;
+    public void setQuantidade(int quantidade) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("Quantidade deve ser maior que zero.");
+        }
+        this.quantidade = quantidade;
+    }
+
+    public double getSubtotal() {
+        if (produto == null) {
+            return 0.0;
+        }
+        return produto.getPreco() * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{id=" + id +
+                ", produto=" + (produto != null ? produto.getNome() : "null") +
+                ", quantidade=" + quantidade +
+                ", subtotal=" + String.format("%.2f", getSubtotal()) + "}";
     }
 }
